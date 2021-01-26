5bb27c221da6e69dc5f34e5b286a33a6bca3ddad
Jose Veiga <jose.veiga@swimlane.com>
2021-01-26 12:26:34 -0500

    add fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,20 @@ import { Component } from "@angular/core";
   template: `
     <div style="text-align:center" class="content">
       <h1>It's alive!</h1>
+      <h3>Not quite...</h3>
     </div>
     <router-outlet></router-outlet>
   `,
-  styles: [],
+  styles: [
+    `
+      h1 {
+        font-family: "Retro Gaming";
+      }
+
+      h3 {
+        font-family: "Montserrat";
+      }
+    `,
+  ],
 })
 export class AppComponent {}
